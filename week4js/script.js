580d208f59c699ba4f510d2bd6e329937ac7e313
upeshchalise <upeshchalise@gmail.com>
2023-01-01 16:08:39 +0545

    practiced

--- week4js/script.js
+++ week4js/script.js
@@ -1,16 +1,47 @@
-var message = 'in global';
-console.log('global: message =' + message);
+// var message = 'in global';
+// console.log('global: message =' + message);
 
-var a = function ( ) {
-    var message = "inside a";
-    console.log('a:message =' + message);
+// var a = function ( ) {
+//     var message = "inside a";
+//     console.log('a:message =' + message);
 
-    function b () {
-        console.log("b.message = " + message);
-    }
-    b()
+//     function b () {
+//         console.log("b.message = " + message);
+//     }
+//     b()
     
+// }
+
+
+// a()
+
+
+
+// ** If statement (all false);
+
+// if (false || NaN || 0 || "" || undefined) {
+//     console.log("this line won't execute");
+// }
+// else {
+//     console.log("all false");
+// }
+
+// // all true
+// if (true && "hello" && 1 && -1 && "false"){
+// console.log("all true");
+// }
+
+// loop in javascript
+var sum = 0;
+for( var i = 0; i<10; i++) {
+console.log(i);
+sum = sum + i;
 }
+console.log("sum upto 9 is " + sum);
 
-
-a()
+function chickenWithDish(withdish) {
+    withdish = withdish || "whatever";
+    console.log("chicken with " + withdish);
+}
+chickenWithDish("noodles");
+chickenWithDish();
